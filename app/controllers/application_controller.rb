9f56b627eeab0314af8aa21e288bb5d047c873f6
Chris Kay <ckay84@protonmail.com>
2020-10-12 14:47:57 -0400

    Update gemfile for api, add timestamps to user migrtn, dlt helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,4 @@
 class ApplicationController < ActionController::API
-
-  helper_method :current_user, :logged_in? 
-
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end 
-
-  def logged_in?
-    !!session[:user_id]
-  end 
-
-  def redirect_if_not_logged_in
-    if !logged_in?
-      flash[:message] = "You must be logged in to view that page."
-      redirect_to root_path
-    end 
-  end 
-
-  def authorized?
-    if current_user.id != params[:id].to_i
-      flash[:message] = "You are not authorized to view that page."
-      redirect_to user_path(current_user) 
-    end
-  end 
+ 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,8 @@ ActiveRecord::Schema.define(version: 2020_10_08_161723) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
