76a6caf3ae42469d91f4ba54a1873df875917566
Chris Kay <ckay84@protonmail.com>
2020-10-12 01:04:53 -0400

    Add strong params and create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,19 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
 
-  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: {error: 'Error creating new user!'}
+    end 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
 
 end
