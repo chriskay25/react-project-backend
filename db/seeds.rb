fcf195313bfdde2a338fc78a108d49a0e5a46136
Chris Kay <ckay84@protonmail.com>
2020-10-15 23:48:48 -0400

    Add simple seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "chris", password: "kay")
+User.create(username: "rick", password: "sanchez")
+User.create(username: "keanu", password: "reeves")
