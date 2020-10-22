6b12f3f55d49aa08fb652780f51e6ceac7f1b1f4
Chris Kay <ckay84@protonmail.com>
2020-10-21 20:05:26 -0400

    Nest game index and show routes under users

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       post '/login', to: 'sessions#create'
       # delete '/logout' to: 'sessions#destroy'
-      resources :users
+      resources :users do
+        resources :games, only: [:index, :show]
+      end
     end
   end 
 end
