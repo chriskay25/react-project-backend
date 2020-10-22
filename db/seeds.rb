16ceb0a628f8630b6f83ed77676f9fa7a7bbcbdc
Chris Kay <ckay84@protonmail.com>
2020-10-21 22:58:02 -0400

    Add games to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(username: "chris", password: "kay")
-User.create(username: "rick", password: "sanchez")
-User.create(username: "keanu", password: "reeves")
+u1 = User.create(username: "chris", password: "kay")
+u2 = User.create(username: "rick", password: "sanchez")
+u3 = User.create(username: "keanu", password: "reeves")
+
+g1 = u1.games.create(score: 1000)
+g2 = u1.games.create(score: 1200)
+g3 = u1.games.create(score: 1300)
+g4 = u2.games.create(score: 900)
+g5 = u2.games.create(score: 2000)
