8a749cf03e663508145af901945efc734e7bbb2e
Chris Kay <ckay84@protonmail.com>
2020-10-21 20:00:29 -0400

    Add explicit statuses for json rendering

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::SessionsController < ApplicationController
   def create
     @user = User.find_by(username: params[:user][:username])
     if @user && @user.authenticate(params[:user][:password])
-      render json: @user
+      render json: @user, status: :ok
     else
       render json: {
         error: "Invalid credentials",
