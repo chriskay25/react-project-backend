068837278abeffb5e0f82b5c313255b80e1ae9db
Chris Kay <ckay84@protonmail.com>
2020-10-23 17:29:54 -0400

    Build create action & strong params for games_controller, helpers for app_controller

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -6,7 +6,22 @@ class Api::V1::GamesController < ApplicationController
   end
 
   def create
+    if logged_in?
+      game = current_user.games.build(game_params)
+      if game.save
+        render json: game, status: :ok
+      else
+        render json: game.errors, status: :unprocessable_entity
+      end
+    else
+      render json: {error: "You must be logged in to start a new game."}
+    end
+  end
 
+  private
+
+  def game_params
+    params.require(:game).permit(:score, :user_id)
   end
 
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::API
- 
+
+  def current_user
+    user = User.first
+  end 
+
+  def logged_in?
+    true
+  end
 
 end
