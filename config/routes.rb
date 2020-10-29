5f06cc3479bdf2e90db937525e66f2b5d9e1e81e
Chris Kay <ckay84@protonmail.com>
2020-10-29 17:50:46 -0400

    Add route to get currently logged in user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post '/login', to: 'sessions#create'
+      get '/get_current_user', to: 'sessions#get_current_user'
       # delete '/logout' to: 'sessions#destroy'
       resources :users do
         resources :games, only: [:index, :show]
