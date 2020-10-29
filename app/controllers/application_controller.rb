5045f25ef8e3fc9a7599771070c4581fef138e07
Chris Kay <ckay84@protonmail.com>
2020-10-29 17:51:45 -0400

    Add helper methods for auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,27 @@
 class ApplicationController < ActionController::API
 
-  def current_user
-    user = User.first
+  def user_serializer(user)
+    {
+      id: user.id,
+      username: user.username,
+      password: user.password
+    }
+  end
+
+  def logged_in_user
+    User.find_by(id: decode_token_and_get_user_id)
   end 
 
   def logged_in?
-    true
+    !!logged_in_user
+  end
+
+  def generate_token(payload)
+    token = JWT.encode(payload, 'token')
+  end
+
+  def decode_token_and_get_user_id
+    JWT.decode(request.headers['Authorization'], 'token')[0]["id"]
   end
 
 end
