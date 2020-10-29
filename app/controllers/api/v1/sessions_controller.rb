90ec31e3049e87a3c4978e4689da9f4f71356748
Chris Kay <ckay84@protonmail.com>
2020-10-29 17:53:20 -0400

    Generate JWT token in create action, add get_current_user action

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,13 +3,21 @@ class Api::V1::SessionsController < ApplicationController
   def create
     @user = User.find_by(username: params[:user][:username])
     if @user && @user.authenticate(params[:user][:password])
-      render json: @user, status: :ok
+      token = generate_token({ id: @user.id })
+      render json: {
+        user: @user,
+        jwt: token
+      }, status: :ok
     else
       render json: {
         error: "Invalid credentials",
         details: @user.errors.full_messages
       }, status: :unauthorized
     end
+  end
+ 
+  def get_current_user
+    render json: logged_in_user
   end
 
   # def delete
