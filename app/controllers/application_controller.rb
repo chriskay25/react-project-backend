d0794ec2472073ae8e40ef0ee0a3b0b6991bab35
Chris Kay <ckay84@protonmail.com>
2020-10-30 23:46:59 -0400

    Add games to user_serializer helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,24 @@
 class ApplicationController < ActionController::API
 
   def user_serializer(user)
-    {
-      id: user.id,
-      username: user.username,
-      password: user.password
-    }
+    user_games = user.games.map do |g|
+      {
+        id: g.id,
+        score: g.score,
+        user_id: g.user_id
+      }
+    end 
+    
+      {
+        id: user.id,
+        username: user.username,
+        password: user.password,
+        games: user_games
+      }
   end
 
   def logged_in_user
-    User.find_by(id: decode_token_and_get_user_id)
+    @logged_in_user ||= User.find_by(id: decode_token_and_get_user_id)
   end 
 
   def logged_in?
