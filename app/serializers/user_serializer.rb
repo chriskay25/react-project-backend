f02a70f01c70cd85b84ea66d8eeadcde2b5f7d82
Chris Kay <ckay84@protonmail.com>
2020-11-05 16:22:53 -0500

    Add highScore to User AMS

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :password
+  attributes :id, :username, :password, :highScore
   has_many :games
+
+  def highScore
+    object.games.sort {|game| game.score}.first.score
+  end
 end
