57fe776b3137d1207b9292d4bc7589af17054423
Chris Kay <ckay84@protonmail.com>
2020-12-01 18:39:25 -0500

    Generate JWT token when user is created

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -14,7 +14,11 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user, status: :created
+      token = generate_token({ id: @user.id })
+      render json: {
+        user: user_serializer(@user), 
+        jwt: token
+      }, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end 
