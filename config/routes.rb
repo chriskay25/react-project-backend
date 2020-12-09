780d106fa51dee0387fddabeb1757263102b4b2e
Chris Kay <ckay84@protonmail.com>
2020-12-08 23:51:24 -0500

    Add high_score route

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
     namespace :v1 do
       post '/login', to: 'sessions#create'
       get '/get_current_user', to: 'sessions#get_current_user'
+      get '/high_scores', to: 'games#high_scores'
       # delete '/logout' to: 'sessions#destroy'
       resources :users do
         resources :games, only: [:index, :show]
       end
-      resources :games, only: [:index]
+      resources :games, only: [:index, :create]
     end
   end 
 end
