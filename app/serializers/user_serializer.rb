3f184aeed2054dc57088bb2c64776818628f1490
Chris Kay <ckay84@protonmail.com>
2020-12-09 23:46:03 -0500

    Correction - use sort_by

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -3,6 +3,6 @@ class UserSerializer < ActiveModel::Serializer
   has_many :games
 
   def highScore
-    object.games.length > 0 ? object.games.sort {|g| g.score}.first.score : 0
+    object.games.length > 0 ? object.games.sort_by {|g| g.score}.last.score : 0
   end
 end
