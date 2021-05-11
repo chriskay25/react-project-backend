05e5d3fe3728eb2675f1ca212d5fd4da8cd8fad2
Chris Kay <ckay84@protonmail.com>
2021-05-10 23:36:40 -0400

    Add a highScores attribute to serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,13 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :password, :highScore
+  attributes :id, :username, :password, :highScore, :highScores
   has_many :games
 
   def highScore
     object.games.length > 0 ? object.games.sort_by {|g| g.score}.last.score : 0
+  end
+
+  def highScores
+    sorted_games = object.games.sort_by { |game| game.score }.reverse
+    sorted_games.first(5)
   end
 end
